e791d69610e67dd8600ed1d33b88942cf61ef2aa
Nahuel Acosta <nahuelacosta488@gmail.com>
2025-10-29 10:44:26 -0300

    segundo commit

---
+++ src/components/PasswordGenerator.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import { Button, Form } from 'react-bootstrap'
+
+const PasswordGenerator: React.FC = () => {
+  const [length, setLength] = useState<number>(12)
+  const [includeUppercase, setIncludeUppercase] = useState<boolean>(true)
+  const [includeNumbers, setIncludeNumbers] = useState<boolean>(true)
+  const [includeSymbols, setIncludeSymbols] = useState<boolean>(false)
+  const [password, setPassword] = useState<string>('')
+
+  const generatePassword = () => {
+    const lowerChars = 'abcdefghijklmnopqrstuvwxyz'
+    const upperChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const numberChars = '0123456789'
+    const symbolChars = '!@#$%^&*()_-+=<>?'
+
+    let charset = lowerChars
+    if (includeUppercase) charset += upperChars
+    if (includeNumbers) charset += numberChars
+    if (includeSymbols) charset += symbolChars
+
+    let newPassword = ''
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length)
+      newPassword += charset[randomIndex]
+    }
+
+    setPassword(newPassword)
+  }
+
+  return (
+    <div className="card p-4 shadow">
+      <Form>
+        <Form.Group className="mb-3">
+          <Form.Label>Largo: {length}</Form.Label>
+          <Form.Range
+            min={6}
+            max={30}
+            value={length}
+            onChange={(e) => setLength(Number(e.target.value))}
+          />
+        </Form.Group>
+
+        <Form.Check
+          type="checkbox"
+          label="Incluir mayúsculas"
+          checked={includeUppercase}
+          onChange={() => setIncludeUppercase(!includeUppercase)}
+        />
+        <Form.Check
+          type="checkbox"
+          label="Incluir números"
+          checked={includeNumbers}
+          onChange={() => setIncludeNumbers(!includeNumbers)}
+        />
+        <Form.Check
+          type="checkbox"
+          label="Incluir símbolos"
+          checked={includeSymbols}
+          onChange={() => setIncludeSymbols(!includeSymbols)}
+        />
+
+        <Button
+          className="mt-3 w-100"
+          variant="primary"
+          onClick={generatePassword}
+        >
+          Generar Contraseña
+        </Button>
+
+        {password && (
+          <div className="alert alert-success mt-3 text-center">
+            <strong>{password}</strong>
+          </div>
+        )}
+      </Form>
+    </div>
+  )
+}
+
+export default PasswordGenerator
